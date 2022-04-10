8b41abb1de71e1551c6d85ebd24eef225126f4ba
Hans-Märten Liiu <hm4543@tlu.ee>
2022-04-10 13:20:47 +0300

    single item page shows item

--- frontend/my-app/src/Pages/SingleItem.js
+++ frontend/my-app/src/Pages/SingleItem.js
@@ -1,17 +1,104 @@
+import React, {useEffect, useState} from "react";
+import Item from "../Components/Item";
 import { Link } from "react-router-dom";
 import NavBar from '../Components/NavBar';
 import './SingleItem.css';
+
+
+
 function SingleItem(){
+    const itemID = window.location.href.split("/item/")[1];
+    //console.log(itemID);
 
+    const [isLoading, setIsLoading] = useState(true);
+    const [product, setProcuct]=useState([]);
+    const [price, setPrice]=useState([]);
+    const [symbol, setSymbol]=useState([]);
+    const [description, setDescription]=useState("");
+    const [pictures, setPictures]=useState([]);
 
-    return(
+    
+    useEffect(()=>{
         
-        <div >
-            <NavBar/>
-            <h1 >Detailed view</h1>
-        </div>
+        fetch('http://localhost:4000/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({
+            query: `
+            query {
+                product(id: "`+itemID+`"){
+                  name
+                  inStock
+                  gallery
+                  description
+                  category
+                  brand
+                  prices{
+                    currency{
+                      label
+                      symbol
+                    }
+                    amount
+                  }
+                   attributes{
+                      id
+                      name
+                      type
+                      items{
+                        displayValue
+                        value
+                        id
+                      }
+                    }
+                  
+                }
+              }`
+        })})
+        .then(res =>  res.json())
+        .then(data =>{
+            console.log(data.data.product);
+            setProcuct(data.data.product);
+            setPrice(data.data.product.prices[0]);
+            setSymbol(data.data.product.prices[0].currency.symbol);
+            let cleanText = data.data.product.description.replace(/<\/?[^>]+(>|$)/g, "");
+            setDescription(cleanText);
+            setPictures(data.data.product.gallery);
+        })
+            
+        
+        setIsLoading(false)
+        
+    },[]);
 
-    )
+    if(isLoading === true){
+        return(
+        <div>
+            Loading...
+        </div>)
+    }else{
+        if(product.inStock == true){
+            return(
+                <div >
+                    <NavBar/>
+                    <h1 >Detailed view</h1>
+                    <h1 >{product.name}</h1>
+                    <h1 >{product.instock}</h1>
+                    <h1 >{product.brand}</h1>
+                    {description}
+                    <h1 >{symbol}{price.amount}</h1>
+                    <img className="itemPicture" src={pictures[0]}></img>
+
+                </div>
+
+            )
+        }else{
+            return(
+                <div>
+                    jou
+                </div>
+            )
+        }
+    }
 }
 
 export default SingleItem;
